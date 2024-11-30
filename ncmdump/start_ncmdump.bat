82177c884ad86b0f6ba864dcdfad18311f3ff817
sakuragmm <1426216116@qq.com>
2024-11-30 15:47:09 +0800

    去除中文

--- ncmdump/start_ncmdump.bat
+++ ncmdump/start_ncmdump.bat
@@ -1,15 +1,14 @@
 @echo off
-chcp 65001 >nul
 for /f "delims=" %%i in ('powershell -NoProfile -Command ^
-  "$folder = (New-Object -ComObject Shell.Application).BrowseForFolder(0, '选择ncm音乐文件夹', 0).Self.Path; if ($folder) { Write-Output $folder }"') do set INPUT_DIR=%%i
+  "$folder = (New-Object -ComObject Shell.Application).BrowseForFolder(0, 'select ncm dir', 0).Self.Path; if ($folder) { Write-Output $folder }"') do set INPUT_DIR=%%i
 
 if "%INPUT_DIR%"=="" (
-    echo 未选文件夹，退出程序...
+    echo ncm dir is null, exit...
     pause
     exit /b
 )
 
-echo 选择的文件夹: %INPUT_DIR%
+echo selected ncm dir: %INPUT_DIR%
 
 
 set OUTPUT_DIR=output
