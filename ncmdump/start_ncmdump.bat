cdc5a88e4da577ffb7679edaf74a1ff1339499b7
guanmm <guanmm@staryea.com>
2024-12-02 14:42:36 +0800

    调整bat

--- ncmdump/start_ncmdump.bat
+++ ncmdump/start_ncmdump.bat
@@ -2,18 +2,21 @@
 for /f "delims=" %%i in ('powershell -NoProfile -Command ^
   "$folder = (New-Object -ComObject Shell.Application).BrowseForFolder(0, 'select ncm dir', 0).Self.Path; if ($folder) { Write-Output $folder }"') do set INPUT_DIR=%%i
 echo selected ncm dir: %INPUT_DIR%
-
-for /f "delims=" %%i in ('powershell -NoProfile -Command ^
-  "$folder = (New-Object -ComObject Shell.Application).BrowseForFolder(0, 'select output dir', 0).Self.Path; if ($folder) { Write-Output $folder }"') do set OUTPUT_DIR=%%i
-
 if "%INPUT_DIR%"=="" (
     echo ncm dir is null, exit...
     pause
     exit /b
 )
+
+for /f "delims=" %%i in ('powershell -NoProfile -Command ^
+  "$folder = (New-Object -ComObject Shell.Application).BrowseForFolder(0, 'select output dir', 0).Self.Path; if ($folder) { Write-Output $folder }"') do set OUTPUT_DIR=%%i\output
 echo selected out put dir: %OUTPUT_DIR%
+if "%OUTPUT_DIR%"=="" (
+    echo output dir is null, exit...
+    pause
+    exit /b
+)
 
 ncmdump.exe -d "%INPUT_DIR%" -o "%OUTPUT_DIR%"
-
 
 pause
